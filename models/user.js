3b291e5839e615700fb2302f92225e3da56dfdf0
leoni <leonimega@hotmail.com>
2017-12-16 11:18:43 -0200

    add bcrypt to salt and hash pw

--- models/user.js
+++ models/user.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const bcrypt = require("bcrypt-nodejs");
 const Schema = mongoose.Schema;
 
 // define model
@@ -6,6 +7,25 @@ const Schema = mongoose.Schema;
 const userSchema = new Schema({
   email: {type: String, unique: true, lowercase: true},
   password: String
+});
+
+// On save Hook, encrypt password
+userSchema.pre("save", function(next){ //called before .save()
+  const user = this;
+  bcrypt.genSalt(10, function(err, salt){ // generate salt
+    if(err){
+      return next(err);
+    }
+    bcrypt.hash(user.password, salt, null, function(err, hash){ //generate hash
+      if(err){
+        return next(err);
+      }
+
+      //replace plain text pw with the hash
+      user.password = hash;
+      next();
+    })
+  })
 });
 
 // creaste the model class
---
+++ readme.md
@@ -0,0 +1,6 @@
+# Server-Authentication
+
+An example of an express server that uses:
+* Token based authentication
+* hash and salt for passwords
+* mongodb and mongoose
