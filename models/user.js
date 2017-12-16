bbe441c496c0a2a6683dfb1bd58606f9a912d4de
leoni <leonimega@hotmail.com>
2017-12-16 14:04:51 -0200

    add method to compare password in user model

--- models/user.js
+++ models/user.js
@@ -28,6 +28,18 @@ userSchema.pre("save", function(next){ //called before .save()
   })
 });
 
+userSchema.methods.comparePassword = function(candidatePassword, callback){
+  // bcrypt uses the candidatePassword and the stored salt (stored in db together with the hash pw)
+  // to generate the hash of the candidatePassword and compare it with the hash stored in the DB
+  bcrypt.compare(candidatePassword, this.password, function(error, isMatch){
+    if(error){
+      return callback(error);
+    }
+
+    callback(null, isMatch)
+  })
+}
+
 // creaste the model class
 const ModelClass = mongoose.model("user", userSchema); //collection named user
 
