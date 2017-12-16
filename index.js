deb766862e6b12d9504e83c692821fca04c908e7
leoni <leonimega@hotmail.com>
2017-12-16 09:01:08 -0200

    first commit

---
+++ index.js
@@ -0,0 +1,19 @@
+//Main starting point of the application
+const express = require("express");
+const http = require("http");
+const bodyParser = require("body-parser");
+const morgan = require("morgan");
+const app = express();
+
+// middlewares:
+// log incoming requests
+app.use(morgan("combined"));
+
+// extract the request body, turn it into a json and exposes it on req.body
+app.use(bodyParser.json({type: "*/*"}));
+
+// server setup
+const port = process.env.PORT || 3090
+const server = http.createServer(app);
+server.listen(port);
+console.log("Server listening on:", port);
