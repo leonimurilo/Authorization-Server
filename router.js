6954450ebfcbf3d9d5824ef72eddf85929d1b37c
leoni <leonimega@hotmail.com>
2017-12-23 10:08:42 -0200

    add get home route

--- router.js
+++ router.js
@@ -8,6 +8,9 @@ const requireAuth = passport.authenticate("jwt", {session: false});
 const requireSignin = passport.authenticate("local", {session: false});
 
 module.exports = function(app){
+  app.get("/", requireAuth, function(req, res){
+    res.send({message: "ABC123"})
+  });
   app.post("/signin", requireSignin, Authentication.signin);
   app.post("/signup", Authentication.signup);
 }
